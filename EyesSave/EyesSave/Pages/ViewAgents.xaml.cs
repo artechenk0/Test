35e8c69785c0ff6fad0f6cde2cc8130dee5b49d9
artcnkk <artchenk0@gmail.com>
2022-12-23 05:04:19 +0300

    first commit

---
+++ EyesSave/EyesSave/Pages/ViewAgents.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using EyesSave.Entities;
+
+namespace EyesSave.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewAgents.xaml
+    /// </summary>
+    public partial class ViewAgents : Page
+    {
+
+        public ViewAgents()
+        {
+            InitializeComponent();
+            SetTypeBox();
+            SetSortBox();
+            UpdateSource();
+        }
+
+        private void SetTypeBox()
+        {
+            var types = EyesSaveEntities.GetConnect().AgentType.ToList();
+
+            types.Insert(0, new AgentType()
+            {
+                Title = "Все типы"
+            });
+
+            TypeBox.ItemsSource = types;
+
+            TypeBox.SelectedIndex = 0;
+        }
+        private void SetSortBox()
+        {
+            var source = new string[] { "Приоритет(убыв.)", "Приоритет(возв.)" };
+
+            SortBox.ItemsSource = source;
+            SortBox.SelectedIndex = 0;
+        }
+
+        private void UpdateSource()
+        {
+            var agents = EyesSaveEntities.GetConnect().Agent.ToList();
+
+            if (TypeBox.SelectedIndex > 0)
+            {
+                agents = agents.Where(x => x.AgentType.Equals(TypeBox.SelectedItem as AgentType)).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(SearchBox.Text))
+            {
+                agents = agents.Where(x => x.Title.ToLower().Contains(SearchBox.Text.ToLower()) || x.Phone.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+            }
+
+            switch (SortBox.SelectedValue)
+            {
+                case "Приоритет(убыв.)":
+                    agents = agents.OrderByDescending(x => x.Priority).ToList();
+                    break;
+                case "Приоритет(возв.)":
+                    agents = agents.OrderBy(x => x.Priority).ToList();
+                    break;
+            }
+
+            AgentsLV.ItemsSource = agents;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateSource();
+        }
+
+        private void TypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSource();
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSource();
+        }
+
+        private void BtnAdd_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Windows.AddEditPage addEditPage = new Windows.AddEditPage();
+            addEditPage.ShowDialog();
+        }
+    }
+}
---
+++ EyesSave/EyesSave/Windows/AddEditPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EyesSave.Entities;
+
+namespace EyesSave.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Window
+    {
+        private Agent _currentAgent = new Agent();
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentAgent;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                EyesSaveEntities.GetConnect().Agent.Add(_currentAgent);
+            }
+            catch (Exception mes)
+            {
+                MessageBox.Show(mes.ToString());
+            }
+        }
+    }
+}
